b08b5559fad75d46eb0394e7889d26d1f073027f
Arvind Rao <arvsrao@gmail.com>
2013-02-03 19:56:15 -0800

    cleaned up project folder. deleted extraneous testing code from main.cpp and removed maintest.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@
 #include <iostream>
 #include "spherepoints.h"
 #include "sphere_vector.h"
-#include <vector>
-#include <list>
 
 int main(int argc, char ** argv)
 {
@@ -27,25 +25,5 @@ int main(int argc, char ** argv)
     //std::cout<< "The points after 10 iterations of gradient descent.\n\n";
     x.print_points();
     
-    /**************/
-    
-//    std::vector<int> blah = {1,2,3,4,5,6,7,8,9,10};
-//    std::list<int> mylist;
-//    
-//    for(auto & x : blah)
-//    {
-//        mylist.push_back(x);
-//    }
-//    
-//    int myint = 11;
-//    int a = mylist.front();
-//    mylist.pop_front();
-//    mylist.push_back(myint); 
-//    
-//    std::cerr<< "adding " << myint << " to the back and popping "<< a << "  from the front.\n";
-//    for(auto &x : mylist)
-//    {
-//        std::cout<< x <<"\n";
-//    }
     return 0;
 }
